b3201d1b9073b53ceee5a86b0446e18cee24293c
Filip Marko <filip.lindman.marko@gmail.com>
2021-06-15 20:17:41 +0200

    fix?

--- lib/main.dart
+++ lib/main.dart
@@ -35,40 +35,81 @@ class TestScreen extends StatelessWidget {
             switch (column) {
               case 1:
                 return Tile(
+                  color: Colors.orange,
                   child: SvgPicture.asset(
                     'images/test_rectangle.svg',
                     fit: fit,
+                    // height: 25,
+                    // width: 25,
                   ),
                 );
               case 2:
                 return Tile(
+                  color: Colors.orange,
                   child: Image.asset(
                     'images/test_rectangle.png',
                     fit: fit,
+                    // height: 25,
+                    // width: 25,
                   ),
                 );
               case 3:
                 return Tile(
                   child: SvgPicture.asset(
                     'images/tall_test_rectangle.svg',
-                    height: 200,
-                    width: 50,
                     fit: fit,
+                    // height: 25,
+                    // width: 25,
                   ),
                 );
               case 4:
                 return Tile(
                   child: Image.asset(
                     'images/tall_test_rectangle.png',
-                    height: 200,
-                    width: 50,
                     fit: fit,
+                    // height: 25,
+                    // width: 25,
                   ),
                 );
               default:
                 return GridHeader(fit.toString());
             }
           },
+        ),
+      ),
+    );
+  }
+}
+
+class Tile extends StatelessWidget {
+  const Tile({
+    Key? key,
+    required this.child,
+    this.color = Colors.blue,
+  }) : super(key: key);
+
+  final Widget child;
+  final Color color;
+
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: ColoredBox(
+        color: color,
+        child: SizedBox(
+          height: 300,
+          width: 300,
+          child: Center(
+            child: ConstrainedBox(
+              constraints: BoxConstraints(
+                minWidth: 100,
+                maxWidth: 100,
+                minHeight: 10,
+                maxHeight: 100,
+              ),
+              child: child,
+            ),
+          ),
         ),
       ),
     );
@@ -89,35 +130,50 @@ class GridHeader extends StatelessWidget {
   }
 }
 
-class Tile extends StatelessWidget {
-  const Tile({
-    Key? key,
-    required this.child,
-  }) : super(key: key);
 
-  final Widget child;
+// @override
+//   Widget build(BuildContext context) {
+//     late Widget child;
+//     if (_picture != null) {
+//       final Rect viewport = Offset.zero & _picture!.viewport.size;
 
-  @override
-  Widget build(BuildContext context) {
-    return Center(
-      child: ColoredBox(
-        color: Colors.blue,
-        child: SizedBox(
-          height: 300,
-          width: 300,
-          child: Center(
-            child: ConstrainedBox(
-              constraints: BoxConstraints(
-                minWidth: 5,
-                maxWidth: 300,
-                minHeight: 300,
-                maxHeight: 300,
-              ),
-              child: child,
-            ),
-          ),
-        ),
-      ),
-    );
-  }
-}
+//       child = SizedBox(
+//         width: widget.width,
+//         height: widget.height,
+//         child: FittedBox(
+//           fit: widget.fit,
+//           alignment: widget.alignment,
+//           clipBehavior: widget.clipBehavior,
+//           child: SizedBox.fromSize(
+//             size: viewport.size,
+//             child: RawPicture(
+//               _picture,
+//               matchTextDirection: widget.matchTextDirection,
+//               allowDrawingOutsideViewBox: widget.allowDrawingOutsideViewBox,
+//             ),
+//           ),
+//         ),
+//       );
+
+//       if (widget.pictureProvider.colorFilter == null &&
+//           widget.colorFilter != null) {
+//         child = UnboundedColorFiltered(
+//           colorFilter: widget.colorFilter,
+//           child: child,
+//         );
+//       }
+//     } else {
+//       child = widget.placeholderBuilder == null
+//           ? _getDefaultPlaceholder(context, widget.width, widget.height)
+//           : widget.placeholderBuilder!(context);
+//     }
+//     if (!widget.excludeFromSemantics) {
+//       child = Semantics(
+//         container: widget.semanticsLabel != null,
+//         image: true,
+//         label: widget.semanticsLabel == null ? '' : widget.semanticsLabel,
+//         child: child,
+//       );
+//     }
+//     return child;
+//   }
